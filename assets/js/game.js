e08f9ebfd47a2bdcaa50de21f636e440e1963992
alexanstagr <estrosx@gmail.com>
2025-07-20 16:46:10 +0300

    basic structure

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-// empty for now
+let currentPlayer = "X";
+
+let board = ["", "", "", "", "", "", "", "", ""];
+
+
+const combinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
+
+const cells = document.querySelectorAll(".thesis");
+const winner = document.getElementById("winner");
+const btnReset = document.getElementById('reset')
+
